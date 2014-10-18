0154664d29f580af2cf32da19f229c48ad0b90c1
Sam Agnew <sagnew@eden.rutgers.edu>
2014-10-18 13:39:59 -0400

    Add fish plays pokemon

---
+++ fpp.js
@@ -0,0 +1,26 @@
+var request = require('request');
+
+module.exports = function(callback) {
+    request.get('http://api.fishplayspokemon.com/position', function(err, data ) {
+        if(err) {
+            console.log(err);
+        }
+        console.log(data.body);
+
+        var pizzaNum;
+        if(data.body.x > 50 && data.body.y > 50) {
+            console.log(0);
+            pizzaNum = 0;
+        } else if(data.body.x < 50 && data.body.y < 50) {
+            console.log(1);
+            pizzaNum = 1;
+        } else if(data.body.x < 50 && data.body.y > 50) {
+            console.log(2);
+            pizzaNum = 2;
+        } else {
+            console.log(3);
+            pizzaNum = 3;
+        }
+        callback(pizzaNum);
+    });
+};
