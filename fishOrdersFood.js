5d9e1419d3fc8069a01b5c1e640ea5d5432c702e
Sam Agnew <sagnew@eden.rutgers.edu>
2014-10-18 14:05:51 -0400

    I know I am pushing my API key

--- fishOrdersFood.js
+++ fishOrdersFood.js
@@ -2,22 +2,22 @@ var ordrin = require("ordrin-api"),
     fpp = require('./fpp'),
     possiblePizzas = require('./possiblePizzas'),
     orderDetails = require('./orderDetails'),
-    ordrinApi = new ordrin.APIs('', ordrin.TEST),
+    ordrinApi = new ordrin.APIs('orqweJcnpgD4mxVRPKRTGAVbTGab33DlqqEDllP4Bck', ordrin.TEST),
 
     //TaTa's pizza rid
     rid = '24605',
-    orderPizza = function(pizzaNum) {
-        orderDetails.rid = rid;
-        orderDetails.tray = possiblePizzas[pizzaNum].tray;
-        ordrinApi.order_guest(orderDetails, function(error, data) {
-            if(error) {
-                console.log(error);
-                process.exit(0);
-            }
+    orderPizzas = function(pizzaNum) {
+      orderDetails.rid = rid;
+      pizza = possiblePizzas[pizzaNum];
+      orderDetails.tray = pizza.tray;
+      ordrinApi.order_guest(orderDetails, function(err, data) {
+        if(err) {
+          console.log(err);
+        }
 
-            console.log("You just ordered " + orderDetails.name);
-            console.log(data);
-        });
+        console.log('You just ordered ' + pizza.name);
+        console.log(data);
+      });
     };
 
-fpp(orderPizza);
+fpp(orderPizzas);
